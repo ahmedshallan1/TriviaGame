5cbeb88af674ed6fac1367d7e9c0ad45b210bc90
Ahmed Shallan <ahmedshallan@Ahmeds-MacBook-Air.local>
2019-10-01 00:35:43 -0400

    Add existing file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var counter = 10;
+var counter = 5;
 var currentQuestion = 0;
 var score = 0;
 var lost = 0;
@@ -7,24 +7,62 @@ var interval;
 var question = quizQuestions[currentQuestion].question;
 var choices = quizQuestions[currentQuestion].choices;
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
-  $(document).on("click", ".choice", function() {
+  const quizQuestions = [
+    {
+      question: "which team won the last world cup?",
+      choices: ["Brazil", "France", "Germany", "Spain"],
+      correctAnswer: "France"
+    },
+
+    {
+      question: "How many times did this team win the world cup?",
+      choices: ["1", "2", "3", "4"],
+      correctAnswer: "2"
+    },
+
+    {
+      question: "who won the first world cup?",
+      choices: ["Mexico", "Spain", "Uruguay", "Brazil"],
+      correctAnswer: "Uruguay"
+    },
+
+    {
+      question: "when was the first world cup?",
+      choices: ["1930", "1934", "1940", "1944"],
+      correctAnswer: "1930"
+    },
+
+    {
+      question: "when was the first world cup?",
+      choices: ["1930", "1934", "1940", "1944"],
+      correctAnswer: "1930"
+    },
+
+  ];
+
+  $(document).on("click", ".choice", function () {
     var userAnswer = $(this).attr("data-answer");
-    if(userAnswer === quizQuestions[currentQuestion].correctAnswer){
+    if (userAnswer === quizQuestions[currentQuestion].correctAnswer) {
       score++;
-      clearInterval(interval); 
-    }else {
+      clearInterval(interval);
+    } else {
       lost++;
       clearInterval(interval);
     }
     currentQuestion++;
-    question = quizQuestions[currentQuestion].question;
-    choices = quizQuestions[currentQuestion].choices;
-    loadQuestion();
+    if(currentQuestion<=quizQuestions.length-1){
+      question = quizQuestions[currentQuestion].question;
+      console.log(question)
+      choices = quizQuestions[currentQuestion].choices;
+      loadQuestion();
+    }
+    
+    
   })
-  
-loadQuestion();
+
+  loadQuestion();
 });
 
 function loadQuestion() {
@@ -43,8 +81,8 @@ function loadQuestion() {
   }
   counter = 10;
   interval = setInterval(countdown, 1000)
-  
-  if ((quizQuestions.length-1) === currentQuestion){
+
+  if ((quizQuestions.length) === currentQuestion) {
     displayResult();
   }
 }
@@ -55,31 +93,35 @@ function countdown() {
   if (counter === 0) {
     lost++;
     currentQuestion++;
+    if(currentQuestion<=quizQuestions.length-1){
     question = quizQuestions[currentQuestion].question;
+    console.log(question)
     choices = quizQuestions[currentQuestion].choices;
     clearInterval(interval);
     loadQuestion();
+    }
+    
+    
   }
 }
 
-function displayResult(){
-  clearInterval(interval);
-  $("#timer").empty();
-  $("#game").empty();
-  $("#options").empty();
-  $("#game").text( "You got " + score + " questions correct");
-  $("#options").text( "You got " + lost + " questions incorrect");  
-}
-
-$("#reset").click(function(){
+$("#reset").click(function () {
   counter = 10;
   currentQuestion = 0;
   score = 0;
   lost = 0;
   interval;
-  
+
   question = quizQuestions[currentQuestion].question;
   choices = quizQuestions[currentQuestion].choices;
   clearInterval(interval);
   loadQuestion();
 });
+function displayResult() {
+  clearInterval(interval);
+  $("#timer").empty();
+  $("#game").empty();
+  $("#options").empty();
+  $("#game").text("You got " + score + " questions correct");
+  $("#options").text("You got " + lost + " questions incorrect");
+}
